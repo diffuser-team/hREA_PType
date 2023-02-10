647c3c123b77afc0f12ddbd7bf4f5e3b3e09f8d7
Aleksandar Gujas <aleksandar.gujas@gmail.com>
2023-02-10 18:55:02 +0100

    feature: :sparkles: create work, no validation on graphql front end

---
+++ apps/rediffusa/apps/web/pages/_app.tsx
@@ -0,0 +1,15 @@
+import type { AppProps } from "next/app";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: process.env.NEXT_PUBLIC_SERVER_URL,
+  cache: new InMemoryCache(),
+});
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
+}
--- apps/rediffusa/apps/web/pages/index.tsx
+++ apps/rediffusa/apps/web/pages/index.tsx
@@ -1,11 +1,79 @@
-import { Button } from "ui";
-import hreaClient from "@vf-ui/graphql-client-holochain";
+import { useForm } from "react-hook-form";
+import { useMutation, useApolloClient, gql } from "@apollo/client";
+
+interface WorkForm {
+  name: string;
+  artist: string;
+  iswc: string;
+  comment: string;
+}
+
+const CREATE_WORKS = gql`
+  mutation CreateWork($input: [WorkCreateInput!]!, $where: ArtistWhere) {
+    createWorks(input: $input) {
+      works {
+        name
+        iswc
+        artist(where: $where) {
+          name
+          gender {
+            name
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default function Web() {
+  const client = useApolloClient();
+  const { register, handleSubmit } = useForm<WorkForm>();
+  const [createWork, { data, loading, error }] = useMutation(CREATE_WORKS);
+
+  const onSubmit = async (data: WorkForm) => {
+    const { name, iswc, artist, comment } = data;
+    const res = await createWork({
+      variables: {
+        input: {
+          name,
+          iswc,
+          comment,
+          artist: {
+            connect: {
+              overwrite: false,
+              where: {
+                node: {
+                  name_CONTAINS: artist,
+                },
+              },
+            },
+          },
+        },
+      },
+    });
+    console.log("sheeeiiiiit");
+    console.log(res);
+  };
+
+  const thing = useMutation(CREATE_WORKS);
+  console.log(thing!);
+
   return (
     <div>
-      <h1>Web</h1>
-      <Button />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <input placeholder="artist" {...register("artist")} />
+          <label>
+            {
+              "Just make sure you type the name in right, I'm not pulling any data here"
+            }
+          </label>
+          <input placeholder="iswc" {...register("iswc")} />
+          <input placeholder="name" {...register("name")} />
+          <input placeholder="comment" {...register("comment")} />
+          <input type="submit" />
+        </div>
+      </form>
     </div>
   );
 }

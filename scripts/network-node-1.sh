f99d4c73978d23ba41722919ea433cb04b02a9ce
Aleksandar Gujas <aleksandar.gujas@gmail.com>
2023-02-13 17:10:46 +1000

    possiblecomit

--- scripts/network-node-1.sh
+++ scripts/network-node-1.sh
@@ -1,4 +1,4 @@
 echo "" | RUST_LOG=info holochain --piped -c conductor-config.yml
-hc sandbox call --running=1234 install-app --app-id=hrea_tester ./bundles/app/full_suite/hrea_suite.happ
+hc sandbox call --running=1234 install-app --app-id=hrea_suite ./bundles/app/full_suite/hrea_suite.happ
 hc sandbox call --running=1234 add-app-ws 8880
 # echo "did it my way"
